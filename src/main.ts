4ac9c228b44ec78f8434e84233916d8c6683eeef
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-12-17 00:41:13 +0200

    improved the code in the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,20 @@ import type { Product } from "./interfaces";
 
 import { ebaySearchCrawl } from "./utils";
 
+// Adjust the search text in this section to update the input for the crawler search.
 const searchText = "running shoes";
 
 const printProductData = (product: Product) => {
     console.log(`Product-${product?.position}:\n${JSON.stringify(product)} \n`);
 };
 
+const printCrawlerResults = ({ productsData, relatedSearchesText }) => {
+    productsData?.forEach((productData: Product) => {
+        printProductData(productData);
+    });
+    console.log(`Related search tags:\n${relatedSearchesText}`);
+};
+
 ebaySearchCrawl(searchText)
-    .then(({ productsData, relatedSearchesText }) => {
-        productsData?.forEach((res) => printProductData(res));
-        console.log(relatedSearchesText);
-    })
+    .then(printCrawlerResults)
     .catch((err) => console.error(err));
